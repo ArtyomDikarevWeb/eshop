b3f59b4f7e7b109ded5bde31719f336f79a94c09
Artyom Dikarev <bellumhon@gmail.com>
2023-05-01 19:26:25 +0400

    pluged css styles

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -5,6 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
+
+        @vite(['resources/css/app.css'])
     </head>
     <body>
         @include('admin.partials.header')
--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -1,3 +1,8 @@
 <header>
-    <div><p>Admin</p></div>
+    <div class="header__container">
+        <div class="header__content">
+            <div><p>Admin</p></div>
+            <div><p class="username">User</p></div>
+        </div>
+    </div>
 </header>
