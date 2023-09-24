bbbff749ba9d57fe57ad33de95fae26282038405
Artyom Dikarev <bellumhon@gmail.com>
2023-09-24 18:32:40 +0400

    feat: integrated scribe to project

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,9 +5,13 @@ namespace App\Http\Controllers\Api;
 use App\Actions\Api\StoreUserAction;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\User\UserStoreRequest;
+use Knuckles\Scribe\Attributes\Endpoint;
+use Knuckles\Scribe\Attributes\Group;
 
+#[Group('User')]
 class UserController extends Controller
 {
+    #[Endpoint('Create user account')]
     public function store(UserStoreRequest $request, StoreUserAction $action): int
     {
         $action($request->dto());
