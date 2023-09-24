db7c0e13006c9beecad3fea5c5abc1e096f57036
Artyom Dikarev <bellumhon@gmail.com>
2023-09-24 19:21:35 +0400

    feat: added bodyParams to docs

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -7,11 +7,13 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\User\UserStoreRequest;
 use Knuckles\Scribe\Attributes\Endpoint;
 use Knuckles\Scribe\Attributes\Group;
+use Knuckles\Scribe\Attributes\Response;
 
 #[Group('User')]
 class UserController extends Controller
 {
     #[Endpoint('Create user account')]
+    #[Response(204)]
     public function store(UserStoreRequest $request, StoreUserAction $action): int
     {
         $action($request->dto());
